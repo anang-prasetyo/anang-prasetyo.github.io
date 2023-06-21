646b613f02291380331b02e9d7d67bf71f46b97d
L200180063 <anprasetyo060@gmail.com>
2023-06-21 20:38:55 +0700

    add skill

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <main>
+  <main class="d-flex flex-column gap-5">
     <section class="container vh-100 d-flex align-items-center py-3">
       <div class="row h-100 w-100 d-flex justify-content-center align-items-center flex-column flex-md-row gap-5">
         <div class="col-12 col-md text-center text-md-start">
@@ -11,12 +11,30 @@
         </div>
       </div>
     </section>
-    <section class="container vh-100">
+    <section class="container min-vh-100">
+      <div class="row">
+        <div class="col-12 p-3 h2 text-center">I'm great at.</div>
+      </div>
+      <div class="col-12">
+        <ul class="row list-unstyled">
+          <li v-for="s, i in skills" :key="i" class="m-auto m-sm-0 col-6 col-sm-4 col-md-3 col-lg-2 py-2">
+            <div class="my-2 d-flex flex-column align-items-center justify-content-center gap-2 p-3 skill">
+              <div class="skill-logo"></div>
+              <div class="">{{ s.name }}</div>
+              <div v-if="s.level == 5" class="text-primary">Excelent</div>
+              <div v-else-if="s.level == 4" class="text-success">Good</div>
+              <div v-else-if="s.level == 3" class="text-warning">Average</div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <section class="container min-vh-100">
       <div class="row">
         <div class="col-12 p-3 h2 text-center">My projects on Github.</div>
         <div class="col-12">
           <ul class="row list-unstyled">
-            <li v-for="p in project" :key="p" class="m-auto m-sm-0 col-8 col-sm-6 col-md-4 col-lg-3 col-lg-2 py-2">
+            <li v-for="p in project" :key="p" class="m-auto m-sm-0 col-8 col-sm-6 col-md-4 col-lg-3 py-2">
               <div class="my-2 project">
                 <div class="project-thumb"></div>
                 <div class="p-2">
@@ -33,6 +51,9 @@
         </div>
       </div>
     </section>
+    <footer class="footer">
+      <div class="container py-3">Is being developed by Anang in 2023</div>
+    </footer>
   </main>
 </template>
 
@@ -73,6 +94,56 @@ const project = ref([
     tag: ['']
   },
 ])
+const skills = ref([
+  {
+    name: 'HTML',
+    level: 5
+  },
+  {
+    name: 'CSS',
+    level: 5
+  },
+  {
+    name: 'JavaScript',
+    level: 4
+  },
+  {
+    name: 'PHP',
+    level: 3
+  },
+  {
+    name: 'Bootstrap',
+    level: 4
+  },
+  {
+    name: 'Vuejs',
+    level: 5
+  },
+  {
+    name: 'MySQL',
+    level: 4
+  },
+  {
+    name: 'Firebase',
+    level: 4
+  },
+  {
+    name: 'Git',
+    level: 4
+  },
+  {
+    name: 'Github',
+    level: 4
+  },
+  {
+    name: 'Codeigniter',
+    level: 3
+  },
+  {
+    name: 'Laravel',
+    level: 3
+  },
+])
 </script>
 
 <style lang="scss" scoped>
@@ -104,6 +175,21 @@ const project = ref([
     &:last-child::after{
       content: '.'
     }
+  }
+}
+.skill{
+  background: whitesmoke; 
+  border-radius: 5px;
+  border: 1px solid black;
+  border-top: 3px solid black;
+  border-left: 3px solid black;
+  &-logo{
+    background: lavender;
+    height: 3rem;
+    width: 3rem;
+    border-radius: var(--border-radius-1);
+    border: var(--border-black);
+    box-shadow: var(--box-shadow-black);
   }
 }
 </style>
