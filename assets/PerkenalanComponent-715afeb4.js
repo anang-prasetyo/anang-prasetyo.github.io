469ed7400db9ab7f64ea7f38e4f8a90c21649b1d
L200180063 <anprasetyo060@gmail.com>
2024-06-10 23:58:26 +0700

    adding dist subtree v2

---
+++ assets/PerkenalanComponent-715afeb4.js
@@ -0,0 +1,1 @@
+import{_ as e,c as n,d as a}from"./index-14ae2414.js";const t={};function o(r,c,s,p,l,_){return n(),a("p",null,"ini adalah tampilan Perkenalan")}const d=e(t,[["render",o]]);export{d as default};
